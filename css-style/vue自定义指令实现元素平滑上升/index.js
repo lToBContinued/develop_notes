4f812ff58b4cc773724822ed136c2d1633f19a1f
ToBContinued <wt3212834434@outlook.com>
2025-02-17 21:22:42 +0800

    调整元素平滑上升代码

--- css-style/vue自定义指令实现元素平滑上升/index.js
+++ css-style/vue自定义指令实现元素平滑上升/index.js
@@ -3,7 +3,7 @@
  * @params {distance} Number 元素上升的距离
  * @params {duration} Number 动画持续时间
  * @example
- *    <div v-slide-in="{distance: 100, duration: 300}"></div>
+ *    <div v-slide-in></div>
  *    <div v-slide-in="{distance: 100, duration: 300}"></div>
  */
 let DISTANCE = 100
@@ -11,7 +11,7 @@ let DURATION = 300
 // 创建一个弱引用的Map对象，用于存储元素和动画之间的映射关系，在不需要时，可以自动释放内存。
 const map = new WeakMap()
 
-// 创建观察器
+// 创建交叉观察器，观察元素和视口有没有重叠
 const ob = new IntersectionObserver((entries) => {
   for (const entry of entries) {
     if (entry.isIntersecting) {
@@ -37,14 +37,19 @@ export const vSlideIn = {
   install(app) {
     app.directive('slide-in', {
       mounted(el, binding) {
-        binding.value ? (DISTANCE = binding.value.distance) : DISTANCE
-        binding.value ? (DURATION = binding.value.duration) : DURATION
+        // 如果用户给了参数，则使用用户的参数
+        if (binding.value) {
+          DISTANCE = binding.value.distance
+          DURATION = binding.value.duration
+        }
         // 如果进入页面时，元素不在视口之下，则不播放动画
         if (!isBelowViewport(el)) {
           return
         }
+        // 创建一个元素的动画
         const animation = el.animate(
           [
+            // ☀ 关键帧，可以在这里修改动画效果 ☀
             {
               transform: `translateY(${DISTANCE}px)`,
               // opacity: 0,
@@ -60,7 +65,7 @@ export const vSlideIn = {
             fill: 'forwards',
           },
         )
-        animation.pause()
+        animation.pause() // 一开始先暂停动画，在进入视口时再播放
         ob.observe(el) // 对元素进行观察
         map.set(el, animation) // 将元素和动画对应起来
       },
