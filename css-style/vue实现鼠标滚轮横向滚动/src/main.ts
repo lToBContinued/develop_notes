c83c3d29c0b27b9d00885992c00c51f8be88d7ea
ToBContinued <wt3212834434@outlook.com>
2025-03-03 20:39:52 +0800

    m

--- css-style/vue实现鼠标滚轮横向滚动/src/main.ts
+++ css-style/vue实现鼠标滚轮横向滚动/src/main.ts
@@ -1,14 +1,8 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
-import router from './router'
-import '@/styles/index.scss'
-import './permission.ts'
 import registerDirectives from '@/directives/index.ts' // 注册自定义指令
 
 const app = createApp(App)
-app.use(createPinia())
-app.use(router)
 registerDirectives(app) // 注册自定义指令
 
 app.mount('#app')
